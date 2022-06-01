1111b88c02cda1bce3f0e772656822f836ea88e8
Bachar Daowd <bachar.daowd@gmail.com>
2022-06-01 16:25:58 +0200

    Heroku Setting

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 const memberRoutes = require('./routes/memberRoutes')
 const tasksRoutes = require('./routes/tasksRoutes')
 const { errorHandler } = require('./middleware/errorMiddleware')
-
+const path = require('path')
 //connect to database
 connectDB()
 
@@ -16,14 +16,23 @@ const app = express()
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(express.json({ limit: '50mb' }))
 
-//Home Route
-app.get('/', (req, res) => {
-  res.status(200).send({ message: 'Welcome' })
-})
 //Routers
 app.use('/api/members', memberRoutes)
 app.use('/api/tasks', tasksRoutes)
 
+//Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  //Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  })
+} else {
+  //Home Route
+  app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Welcome to the task manager application' })
+  })
+}
 
 //Error Handler middleware
 app.use(errorHandler)
